a9ff6f45e9a25f32ff3ad8f4afd732d0b8e7f3a6
zdkstdio <369025818@qq.com>
2026-01-07 19:18:35 +0800

    新建项目

---
+++ Source/DigitalHumanDemo/DigitalHumanDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DigitalHumanDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DigitalHumanDemo, "DigitalHumanDemo" );
